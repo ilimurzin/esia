d452a93a8f2a459770c3109c70fce86fcb2beb28
Vladimir Ilimurzin <ilimurzin@ya.ru>
2024-11-15 22:08:50 +0300

    Добавил php-cs-fixer

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,13 @@
+<?php
+
+return (new \PhpCsFixer\Config())
+    ->setRules([
+        '@PER-CS' => true,
+    ])
+    ->setFinder(
+        (new \PhpCsFixer\Finder())
+            ->in([
+                __DIR__ . '/src/',
+                __DIR__ . '/tests/',
+            ])
+    );
