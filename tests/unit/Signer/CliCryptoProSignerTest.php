5f1d2197127fcc14abbc280b45eb108972347599
Vladimir Ilimurzin <ilimurzin@ya.ru>
2025-01-15 11:17:17 +0300

    feat: verify CLI CryptoPro signature

--- tests/unit/Signer/CliCryptoProSignerTest.php
+++ tests/unit/Signer/CliCryptoProSignerTest.php
@@ -6,16 +6,41 @@ use Esia\Signer\CliCryptoProSigner;
 
 class CliCryptoProSignerTest extends \Codeception\Test\Unit
 {
+    protected function setUp(): void
+    {
+        $output = null;
+        $resultCode = null;
+        // Suppose cryptcp is in your PATH
+        exec('cryptcp -help', $output, $resultCode);
+
+        if ($resultCode !== 0) {
+            $this->markTestSkipped('The cryptcp utility is not available');
+        }
+    }
+
     public function testSign(): void
     {
         $signer = new CliCryptoProSigner(
-            '/opt/cprocsp/bin/amd64/cryptcp',
-            '66821344ce484aceb984d887b303544bfdda8ea4'
+            'cryptcp',
+            '745187e5c161cd2e3130d886f9df4492fa270685',
+            'test'
         );
 
-        // we expect exception here, because we can't install cryptcp to ci 🤷
-        $this->expectException(\Esia\Signer\Exceptions\SignFailException::class);
-        $signer->sign('test');
+        $signature = $signer->sign('test');
+
+        file_put_contents(codecept_log_dir('content'), 'test');
+        $signature = base64_decode(strtr($signature, '-_', '+/'));
+        file_put_contents(codecept_log_dir('signature'), $signature);
+        $command = sprintf(
+            "openssl smime -verify -inform DER -in %s -CAfile %s -content %s",
+            codecept_log_dir('signature'),
+            codecept_data_dir('server.crt'),
+            codecept_log_dir('content')
+        );
+        $output = null;
+        $resultCode = null;
+        exec($command, $output, $resultCode);
+        self::assertEquals(0, $resultCode, 'OpenSSL verification failure');
     }
 
     public function testTempDirDoesNotExists(): void
